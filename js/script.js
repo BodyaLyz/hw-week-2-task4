b70a9f4dd8eec8fc9b94a78a26c08ec5be5ec95d
Bodya Lyzen <43245185+BodyaLyz@users.noreply.github.com>
2018-11-03 19:07:51 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+function rec(start, end) {
+	let j = 1;
+	let str = '';
+	while (j <= start) {
+		str = str + '#';
+		j++;
+	}
+	console.log(str);
+	if (start < end) {
+		rec(start+1, end);
+	}
+	return;
+}
+ rec(1, 8);
+
